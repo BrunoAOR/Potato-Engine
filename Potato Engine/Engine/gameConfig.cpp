d0751fa3eb93537aea576566836b2862875ca4c6
Bruno Ortiz <bruno.ortiz.r@gmail.com>
2017-12-02 12:52:20 +0100

    Modify Prefabs system to use Prefab instances
    - Modify the PrefabsFactory::instantiate method to receive Reference<Prefab> as parameter instead of Classes through template
    - BUG FIX: Fix issue in the ReferenceOwner class, where using the move assignment operator would delete the referencesList too early in the move process and leave the new ReferenceOwner in an unuseable state

--- Potato Engine/Engine/gameConfig.cpp
+++ Potato Engine/Engine/gameConfig.cpp
@@ -2,6 +2,7 @@
 
 #include "Engine.h"
 #include "SceneManager.h"
+#include "PrefabsFactory.h"
 
 
 const int SCREEN_SIZE = 2;
@@ -9,20 +10,35 @@ const int SCREEN_WIDTH = 320;
 const int SCREEN_HEIGHT = 224 + 32 / SCREEN_SIZE;
 
 
+std::vector<std::string> renderLayersConfig()
+{
+	return std::vector<std::string>{"Background", "Main", "Foreground" };
+}
+
+
 bool scenesConfig()
 {
 	// Success flag
 	bool success = true;
 
-	// Usage: import and add everyScene as shown in the following line (where SomeScene is a class that inherits from Scene):
+	// Usage: import and add every Scene as shown in the following line (where SomeScene is a class that inherits from Scene):
 	//success &= engine->sceneManager->addScene<SomeScene>();
-
-	engine->sceneManager->loadScene(0);
+	if (success)
+	{
+		engine->sceneManager->loadScene(0);
+	}
 	return success;
 }
 
 
-std::vector<std::string> renderLayersConfig()
+bool prefabsConfig()
 {
-	return std::vector<std::string>{"Background", "Main", "Foreground" };
+	// Success flag
+	bool success = true;
+
+	// Usage: import adn add every Prefab as shown on the following line (where somePrefab is a class that inherits from Prefab):
+	// success &= engine->prefabsFactory->addPrefab<SomePrefab>("SomePrefab");
+	// Note that the parameter in the addPrefab method can be any string, but it should be unique. This string will be used to retrieve the Prefab later on.
+
+	return success;
 }
